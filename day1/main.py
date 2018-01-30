c30ad759a6859155188ae908577cefde8009fed2
Alexandre VIROS <alexandre.viros@live.fr>
2018-01-30 06:02:12 +0100

    Completed day 1

---
+++ day1/main.py
@@ -0,0 +1,20 @@
+"""
+Day 1 - Part 1
+"""
+
+def compute(data, step_forward):
+    res = 0
+    for i in range(len(data)): #len(data) not included in the generated value
+        comparable_index = (i + step_forward) % len(data)
+        if data[i] == data[comparable_index]:
+            res += int(data[i])
+
+    return res
+
+with open("inputs/day1", "r") as f:
+    read_data = f.read()
+f.close()
+
+print "** Day 1 **"
+print "Part1: " + str(compute(read_data, 1))
+print "Part2: " + str(compute(read_data, len(read_data)/2))
